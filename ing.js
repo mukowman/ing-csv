f10f7a1c3d9459df3c42804b2553b3cae56311b7
mukowman <tim@rawalls.com>
2020-05-20 20:44:21 +1000

    Update ing.js

--- ing.js
+++ ing.js
@@ -14,7 +14,16 @@ const { login } = require('ing-au-login');
 
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+        // Required for Docker version of Puppeteer
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        // This will write shared memory files into /tmp instead of /dev/shm,
+        // because Docker’s default for /dev/shm is 64MB
+        '--disable-dev-shm-usage'
+        ]
+    })
     const page = await browser.newPage();
     const authToken = await login(page, CLIENT_NUMBER, ACCESS_CODE)
     await browser.close();
